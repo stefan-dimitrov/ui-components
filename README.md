ce6230343f03ac301248dbb1ec45a7eb4971da6e
Miroslav Genov <mgenov@gmail.com>
2016-11-27 19:31:22 +0200

    build: make react component build using webpack
    A skeleton build using webpack for building ui-components.

---
+++ README.md
@@ -0,0 +1,10 @@
+# ui-components
+React component library for supporting internal and external projects with it's components.
+
+## Development Flow
+
+````bash
+npm run build
+```
+
+
---
+++ source/demo/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+const Application = () => {
+  return (<div>Hello World !</div>)
+}
+
+render(
+  <Application />,
+  document.getElementById('root')
+)
