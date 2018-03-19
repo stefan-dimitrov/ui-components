e71ba25077cac9f813984c497c183011858c5ee9
Martin Milev <martinmariusmilev@gmail.com>
2018-03-19 11:38:55 +0200

    native: added support for icon fonts
    Since in Politis are used different fonts from react native vector
    icons, added support for "MaterialComunity", "Feather", "Octicons" and "SimpleLineIcons".

--- native/src/Icon/Icon.js
+++ native/src/Icon/Icon.js
@@ -1,17 +1,29 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Ionicon from 'react-native-vector-icons/Ionicons'
-import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons'
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons'
+import MaterialCommunityIcon from 'react-native-vector-icons/MaterialCommunityIcons'
 import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome'
+import OctIcon from 'react-native-vector-icons/Octicons'
+import FeatherIcon from 'react-native-vector-icons/Feather'
+import SimpleLineIcon from 'react-native-vector-icons/SimpleLineIcons'
 
 const Icon = ({ name, family, size, color, style }) => {
-  switch(family) {
+  switch (family) {
     case 'Ionicons':
       return <Ionicon name={name} color={color} size={size} style={style} />
-    case 'MaterialCommunityIcons':
+    case 'MaterialIcons':
       return <MaterialIcon name={name} color={color} size={size} style={style} />
+    case 'MaterialCommunityIcons':
+      return <MaterialCommunityIcon name={name} color={color} size={size} style={style} />
     case 'FontAwesome':
       return <FontAwesomeIcon name={name} color={color} size={size} style={style} />
+    case 'Octicons':
+      return <OctIcon name={name} color={color} size={size} style={style} />
+    case 'Feather':
+      return <FeatherIcon name={name} color={color} size={size} style={style} />
+    case 'SimpleLineIcons':
+      return <SimpleLineIcon name={name} color={color} size={size} style={style} />
   }
 }
 
@@ -24,7 +36,7 @@ Icon.propTypes = {
 }
 
 Icon.defaultProps = {
-  family: 'MaterialCommunityIcons',
+  family: 'MaterialIcons',
   size: 20
 }
 
